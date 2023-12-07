a866fc715a57f3629af516eee95191cd4796bd95
ravikim <ravikim3737@gmail.com>
2023-12-07 14:07:20 +0900

    update timestamp(kst) in log

--- renderer/components/hooks/useLog.tsx
+++ renderer/components/hooks/useLog.tsx
@@ -1,15 +1,17 @@
 import { logAtom } from "../../atoms/logAtom";
 import log from "electron-log/renderer";
 import { useSetAtom } from "jotai";
-import React from "react";
 
 const useLog = () => {
   const setLogData = useSetAtom(logAtom);
 
-  const logit = (...args: any) => {
-    log.log(...args);
+  const logit = (...args: any[]) => {
+    //timezone 을 utc로 할거라면 아래 코드를 사용.
+    // const timestamp = new Date().toISOString();
+    const timestamp = new Date().toLocaleString("ko-KR", { timeZone: "Asia/Seoul" });
+    log.log(timestamp, ...args);
 
-    const data = [...args].join(" ");
+    const data = `${timestamp} ${args.map(arg => typeof arg === 'object' ? JSON.stringify(arg) : arg).join(" ")}`;
     setLogData((prevLogData) => [...prevLogData, data]);
   };
 
